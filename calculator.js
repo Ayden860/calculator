531c0aa58369fa18d9ef93e8c436565a3ea72338
Ayden860 <134655473+Ayden860@users.noreply.github.com>
2024-01-25 10:32:45 -0800

    Its combining everything

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
-function addition(boo){
-    boo = '0';
+function addition(){
+    let boo = '0';
     console.log("Press 1 to Add\n");
     console.log("Press 2 to Subtract\n");
     console.log("Press 3 to Multiply\n");
@@ -7,25 +7,49 @@ function addition(boo){
     // var response;
     let many;
     const list = [];
-    // do
-    // {
-       var response = prompt("Enter a Vaue ", "1");
-    // }
-    // while(response > 5 && response >= 1)
-    many = prompt("How many? ")
+    do
+    {
+       var response = prompt("Enter a value ", "1");
+    }
+    while(response > 5 && response >= 1)
+    many = prompt("How many? ", "2")
     for(let i = 0; i < many; i++)
     {
         list[i] = prompt("Your number ")
     }
-    if(response === 1){
+    if(response === '1'){
         for(let m = 0; m < many; m++){
-            boo = boo + list[m];
+            boo += Number(list[m]);
         }
-        return boo;
+        // var msg = boo;
+        // document.getElementById("demo").innerHTML = "10";
+        document.getElementById("demo").innerText = boo;
+        console.log(boo);
+        // return boo;
     }
-
-    const cars = [];
-    return boo;
+    if(response === '2'){
+        for(let p = 0; p < many; p++){
+            boo = boo - list[p];
+        }
+        document.getElementById("demo").innerHTML = boo;
+        // return boo;
+    }
+    if(response === '3'){
+        for(let l = 0; l < many; l++){
+            boo = boo * list[l];
+        }
+        document.getElementById("demo").innerHTML = boo;
+        // return boo;
+    }
+    if(response === '4'){
+        for(let k = 0; k < many; k++){
+            boo = boo / list[k];
+        }
+        document.getElementById("demo").innerHTML = boo;
+        // return boo;
+    }
+    document.getElementById("demo").innerHTML = boo;
+    // return boo;
 }
 
-console.log(addition(1));
+// console.log(addition(1));
